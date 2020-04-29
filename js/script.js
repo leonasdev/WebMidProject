8b66f6d8528d18bd4cea5132dbc277bf0c64d518
ko1596 <ko89022189@gmail.com>
2020-04-29 20:00:41 +0800

    刪除移入

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ $(document).ready(function () {
             if (bottom_of_window > bottom_of_object - 100) {
 
                 $(this).animate({ 'opacity': '1' }, { duration: 500, queue: false });;
-                $(this).animate({ 'left': '0px' }, { duration: 500, queue: false });;
+
             }
 
         });
